7f68a3caad7cbd6137c06cc7375559fdb9f04707
Ernest Nnaji <nectico2@yahoo.com>
2023-09-23 18:11:43 -0400

    Basic JS Exercises 02 Function Parameter

--- practice/02_function_parameters.js
+++ practice/02_function_parameters.js
@@ -1,25 +1,25 @@
 
 // #1 Complete the function by adding the appropriate
 // parameters to the function.
-function double(){ // <-- add code inside the parenthesis ()
+function double(num){ // <-- add code inside the parenthesis ()
     return num * 2
 }
 
 // #2 Complete the function by adding the appropriate
 // parameters to the function.
-function multiply(){ // <-- add code inside the parenthesis ()
+function multiply(a, b){ // <-- add code inside the parenthesis ()
     return a * b;
 }
 
 // #3 Complete the function by adding the appropriate
 // parameters to the function.
-function shout(){ // <-- add code inside the parenthesis ()
+function shout(word){ // <-- add code inside the parenthesis ()
     return word.toUpperCase();
 }
 
 // #4 Complete the function by adding the appropriate
 // parameters to the function.
-function first(){ // <-- add code inside the parenthesis ()
+function first(arr){ // <-- add code inside the parenthesis ()
     return arr[0];
 }
 
